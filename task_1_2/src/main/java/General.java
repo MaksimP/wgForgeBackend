3bb07497a5827de5b9b9d1b0134ff519ac163a04
MaksimP <maksimp@mail.ru>
2019-08-08 20:12:42 +0100

    create Dockerfile for task 1, 2

--- task_1_2/src/main/java/General.java
+++ task_1_2/src/main/java/General.java
@@ -1,14 +1,14 @@
-import java.io.IOException;
-
 public class General {
 
-    private static final String URL = "jdbc:postgresql://localhost:5432/postgres";
-    //private static final String URL = "jdbc:postgresql://192.168.99.100:5432/postgres";
-    private static final String USER = "postgres";
-    private static final String PASSWORD = "1234";
+    //private static final String URL = "jdbc:postgresql://localhost:5432/wg_forge_db";
+    private static final String URL = "jdbc:postgresql://192.168.99.100:5432/wg_forge_db";
+    private static final String USER = "wg_forge";
+    private static final String PASSWORD = "42a";
 
     public static void main(String[] args) {
         DB db = new DB(URL, USER, PASSWORD);
         db.executeDB();
     }
 }
+
+
